5920ff254244810bc6d758fd1959c9ce85b20868
Harry W Jackson <hwjackson1996@gmail.com>
2018-11-10 18:41:04 +0000

    Removed stale comments

--- src/network/Main.java
+++ src/network/Main.java
@@ -2,8 +2,8 @@ package network;
 public class Main {
 	
 	public static void main(String args[]) {
-		Matrix a = new Matrix(4,2); //works
-		a.randomMatrix();		//works
+		Matrix a = new Matrix(4,2); 
+		a.randomMatrix();		
 		a.printMatrix();	
 	}
 
