09ca28bdeed0f1cfac61b35f91a45470458f03e2
Harry W Jackson <hwjackson1996@gmail.com>
2018-11-10 18:36:26 +0000

    Package Refactor.

--- src/network/Main.java
+++ src/network/Main.java
@@ -1,11 +1,10 @@
+package network;
 public class Main {
 	
 	public static void main(String args[]) {
 		Matrix a = new Matrix(4,2); //works
 		a.randomMatrix();		//works
-		a.printMatrix();
-		
-	
+		a.printMatrix();	
 	}
 
 }
--- src/network/Matrix.java
+++ src/network/Matrix.java
@@ -1,3 +1,4 @@
+package network;
 
 import java.util.Arrays;
 
